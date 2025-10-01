f4a39930d713397965c1dab92e54e9155369077e
carlosArmando <carlosramirezsalas400@gmail.com>
2025-09-30 18:36:58 -0700

    añadido de pantalla game over y enemigos en carriles abajo, medio y arriba, tambien implementacion de daño al personaje y enemigos aerios de los carriles

--- Scenes/seleccion_personaje.gd
+++ Scenes/seleccion_personaje.gd
@@ -2,6 +2,6 @@
 extends Control
 
 func _on_pesonaje_pressed():
-	get_tree().change_scene_to_file("res://Scenes/Niveles.tscn")
+	get_tree().change_scene_to_file("res://level.tscn")
 func _on_button_regresar_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/ui.tscn")
--- level.gd
+++ level.gd
@@ -1,118 +1,150 @@
 extends Node2D
-#---------------------precargar las ecenas de obstaculos
-var spikes_scene = preload("res://spikes.tscn")
-var turtle_scene = preload("res://turtle.tscn")
-var bird_scene = preload("res://blue_bird.tscn")
-var rino_scene = preload("res://rino.tscn")
 
-var obstacles_type := [spikes_scene, rino_scene]
-var obstacles : Array
+#--------------------- Precargar las escenas de obstáculos
+var spikes_scene = preload("res://enemigos/spikes.tscn")
+var turtle_scene = preload("res://enemigos/turtle.tscn")
+var bird_scene = preload("res://enemigos/blue_bird.tscn")
+var rino_scene = preload("res://enemigos/rino.tscn")
+var bat_scene = preload("res://enemigos/bat.tscn")
+var bad_pig = preload("res://enemigos/bad_pig.tscn")
+var lane_lower = preload("res://ground_down.tscn")
+var lane_middle = preload("res://ground_mid.tscn")
+var lane_upper = preload("res://ground_top.tscn")
 
-# ---------------- Carriles disponibles (Y)
-var lanes = [95, 206, 320]   # Ajusta estas posiciones según tu nivel
+var lanes = {
+	"lower": 320,   # Carril inferior (cueva)
+	"middle": 206,  # Carril intermedio
+	"upper": 95     # Carril superior (cielo)
+}
 
-#aparicion de obstaculos
-@export var min_gap : int = 300
-@export var max_gap : int = 600
-#----------------GAME VARIABLES
-#const player_start_pos := Vector2i(-260,300)
-#const cam_start_pos := Vector2i(0,180)
+var obstacles_by_lane = {
+	"lower": {
+		"ground": [spikes_scene, rino_scene],
+		"air": [bat_scene]
+	},
+	"middle": {
+		"ground": [spikes_scene, bad_pig],
+		"air": [bat_scene, bird_scene]
+	},
+	"upper": {
+		"ground": [spikes_scene],
+		"air": [bird_scene]
+	}
+}
 
+#----------------- ProgressBAR
+@export var level_Lenght : int = 2000
+var progress : int = 0
+
+#---------------- Carriles disponibles (Y)
+var top_lane := [95]
+var mid_lane := [206]
+var bot_lane := [320]
+
+# Aparición de obstáculos
+@export var min_gap : int = 150
+@export var max_gap : int = 300
+
+#---------------- GAME VARIABLES
 var score : int 
 const Score_modifier : int = 30
-
 var SPEED : float
 
-#----------Velocidad inicial 
+# Velocidad inicial 
 @export var START_SPEED : float = 5.0
 const MAX_SPEED : int = 25
 const Speed_modifier : int = 5000
-var screen_size : Vector2i
 
+var screen_size : Vector2i
 var ground_height : int
 var game_running : bool
 
 var last_obs
+var obstacles : Array = []
 
+#----------------- READY
 func _ready():
 	screen_size = get_window().size
-	
 	ground_height = $ground_down.position.y
 	new_game()
-	
+
 func new_game():
 	score = 0
-	show_Score()
 	game_running = false
-	
-	#$jugador.position = player_start_pos
 	$jugador.velocity = Vector2i(0, 0)
-	#$Camera2D.position = cam_start_pos
-	#$ground_top.position = Vector2i(0, 125)
-	#$ground_down.position = Vector2i(0, 350)
-	#$ground_mid.position = Vector2i(0, 235)
-	
-	$HUD.get_node("ScoreLabel").show()
-	
+
+#----------------- PROCESS
 func _process(delta):
 	if game_running:
-		
 		SPEED = START_SPEED
+		progress += SPEED
+		$HUD.get_node("ProgressBar").value = progress / 50
 		
-		#-------------------------------------------------aumento de velocidad del nivel
-		#SPEED = START_SPEED + score / Speed_modifier
-		#if SPEED > MAX_SPEED:
-			#SPEED = MAX_SPEED
-			
-		#generar obstaculos funcion
-		generate_obs()
-		
+		# Mover jugador, cámara y pared de muerte
 		$jugador.position.x += SPEED
 		$Camera2D.position.x += SPEED
 		$wall_die.position.x += SPEED
 		score += SPEED
-		show_Score()
 		
-		#actualizacion de suelos
-		if $Camera2D.position.x - $ground_down.position.x > screen_size.x * 1.5:
-			$ground_down.position.x += screen_size.x
-		elif $Camera2D.position.x - $ground_top.position.x > screen_size.x * 1.5:
-			$ground_top.position.x += screen_size.x
-		elif $Camera2D.position.x - $ground_mid.position.x > screen_size.x * 1.5:
-			$ground_mid.position.x += screen_size.x
-	else: 
+		#generar obstaculos funcion
+		generate_obs()
+		
+		# Actualización de suelos
+		var ground_down_segments = [$ground_down, $ground_down2]
+		var ground_mid_segments = [$ground_mid, $ground_mid2]
+		var ground_top_segments = [$ground_top, $ground_top2]
+
+		for seg in ground_down_segments:
+			if $Camera2D.position.x - seg.position.x > screen_size.x * 1.5:
+				seg.position.x += screen_size.x * 2
+
+		for seg in ground_mid_segments:
+			if $Camera2D.position.x - seg.position.x > screen_size.x * 1.5:
+				seg.position.x += screen_size.x * 2
+
+		for seg in ground_top_segments:
+			if $Camera2D.position.x - seg.position.x > screen_size.x * 1.5:
+				seg.position.x += screen_size.x * 2
+
+	else:
 		if Input.is_action_pressed("ui_accept"):
 			game_running = true
 			$HUD.get_node("StartLabel").hide()
-			
-			
-@export var max_spawn : int = 3   
+
+#----------------- Obstáculos
+@export var max_spawn : int = 10
 @export var max_obstacles : int = 12
+
 func generate_obs():
-	if last_obs == null or last_obs.position.x < score + randi_range(min_gap, max_gap):
-		# Cuántos obstáculos generar en grupo
-		
+	if last_obs == null or last_obs.position.x < $Camera2D.position.x + randi_range(min_gap, max_gap):
 		var count = randi_range(1, max_spawn)
-		
-		var obs_x_base : int = screen_size.x + score + min_gap
+		var obs_x_base : int = screen_size.x + $Camera2D.position.x + min_gap
 		
 		for i in range(count):
-			var obs_type = obstacles_type[randi() % obstacles_type.size()]
-			var obs = obs_type.instantiate()
+			# Elegir carril al azar
+			var lane_keys = ["lower", "middle", "upper"]
+			var lane_name = lane_keys[randi() % lane_keys.size()]
+			var lane_y = lanes[lane_name]
 			
-			# --- La posición en X se separa con un offset ---
-			var obs_x = obs_x_base + (i * (min_gap + 50))  # <- cada obstáculo se separa del otro
-			var obs_y = lanes[randi() % lanes.size()]
+			# Elegir tipo de obstáculo
+			var type = "air" if randf() < 0.4 else "ground"
+			if obstacles_by_lane[lane_name][type].size() == 0:
+				type = "air"
+			
+			# Instanciar obstáculo
+			var obs_type = obstacles_by_lane[lane_name][type][randi() % obstacles_by_lane[lane_name][type].size()]
+			var obs = obs_type.instantiate()
+			var obs_y = lane_y - 40 if type == "air" else lane_y
+			var obs_x = obs_x_base + (i * (min_gap + 50))
 			
 			last_obs = obs
 			add_obs(obs, obs_x, obs_y)
 
-		
+#----------------- Agregar obstáculo a la escena
 func add_obs(obs, x, y):
 	obs.position = Vector2i(x, y)
 	add_child(obs)
 	obstacles.append(obs)
-		
-func show_Score():
-	#con el get node podemos conseguir los nodos dentre de la ecena de HUD
-	$HUD.get_node("ScoreLabel").text = "SCORE: " + str(score / Score_modifier)
+	if obstacles.size() > max_obstacles:
+		var old = obstacles.pop_front()
+		old.queue_free()
